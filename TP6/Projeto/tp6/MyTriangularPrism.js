87c128a47afde1633223ba6839ea1ea01c551505
julietafrade97 <up201506530@fe.up.pt>
2017-05-21 15:07:50 +0100

    update de trapezios

--- TP6/Projeto/tp6/MyTriangularPrism.js
+++ TP6/Projeto/tp6/MyTriangularPrism.js
@@ -46,7 +46,7 @@ MyTriangularPrism.prototype.display = function() {
     this.scene.popMatrix();
 
     this.scene.pushMatrix();
-    this.scene.translate(2, 0.5, 0.5);
+    this.scene.translate(2.5, 0.5, 0.5);
     this.scene.rotate(Math.PI / 4, 0, 0, 1);
     this.scene.rotate(Math.PI / 2, 0, 1, 0);
     this.scene.scale(1, Math.sqrt(2), 1);
@@ -54,7 +54,7 @@ MyTriangularPrism.prototype.display = function() {
     this.scene.popMatrix();
 
     this.scene.pushMatrix();
-    this.scene.translate(2, 0.5, 0.5);
+    this.scene.translate(-2.5, 0.5, 0.5);
     this.scene.rotate(Math.PI, 0, 1, 0);
     this.scene.rotate(Math.PI / 4, 0, 0, 1);
     this.scene.rotate(Math.PI / 2, 0, 1, 0);
